e9e5c1c8a42821c48346f30c54781677e2a04189
Dhinakar S <dhinakarshanmugam6@gmail.com>
2026-05-01 14:04:18 +0530

    Add fallbacks to NextAuth config

--- src/auth.ts
+++ src/auth.ts
@@ -7,10 +7,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      clientId: process.env.AUTH_GOOGLE_ID || process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET || process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
   },
@@ -22,6 +23,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session;
     },
   },
-  debug: process.env.NODE_ENV === "development",
+  debug: true,
   trustHost: true,
 })
